9c15658e1fa3887b0f310f6fd6281952db929fb9
Vincent Belpaire <vincent.belpaire@gmail.com>
2022-10-03 12:02:58 +0200

    update optics

--- optics1.py
+++ optics1.py
@@ -49,22 +49,20 @@ class Lightbeam:
                 ni, nr = N[i1,j1], N[i2,j2]
                 for k in range(1,100):
                     Un = N[i2-1, j2-k:j2+1+k] # upper row
-                    print("U", Un)
                     if (Un[0] == ni) & (nr in Un):
                         Uj = k-np.min(np.where(Un == nr))
                         break
                 for k in range(1,100):
                     Ln = N[i2+1, j2-k:j2+1+k] # lower row
-                    print("L", Ln)
                     if (Ln[0] == ni) & (nr in Ln):
                         Lj = k-np.min(np.where(Ln == nr))
                         break
-                n = np.array([1, (Lj-Uj)/2])
+                n = np.array([1, -(Uj-Lj)/2])
                 n = n/(n@n)**(0.5) # normalization
                 cosi = n@v
                 thetai = np.degrees(np.arccos(cosi))
                 if thetai > 90:
-                    thethai = thetai%90
+                    thetai = thetai%90
                     n = -n
                 sinr = ni/nr*np.sin(np.radians(thetai))
                 thetar = np.arcsin(sinr)
@@ -78,15 +76,15 @@ import matplotlib.pyplot as plt
 a, b = 1, 2
 H, B = 480, 1000
 h = 10**(-2)
-x0, y0 = B/4*h, H/2*h
+x0, y0 = B/2*h, H/2*h
 geometry = lambda x, y: (abs(((x-x0)/a)**2-((y-y0)/b)**2) <= 1) & (abs(y) <= H*h)
 #geometry = lambda x, y: abs((y-y0)-2*(x-x0)) <= 2
 
 space = Space(h, (H,B))
-n = 1.4
+n = 1.6
 space.add_material(geometry, n)
 
-light = Lightbeam(np.array([1,0]), y0+70*h, h)
+light = Lightbeam(np.array([1,0]), y0+100*h, h)
 L = light.propagate(space, B)
 x, y = L[:, 0]/h, L[:, 1]/h
 N = space.N
