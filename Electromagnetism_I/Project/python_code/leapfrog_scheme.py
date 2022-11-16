6a7613ab671db3655417341f7e0a3ee6f6de5264
Vincent Belpaire <vincent.belpaire@gmail.com>
2022-11-16 12:01:57 +0100

    Code update 16-11-22

--- Electromagnetism_I/Project/python_code/leapfrog_scheme.py
+++ Electromagnetism_I/Project/python_code/leapfrog_scheme.py
@@ -1,36 +1,60 @@
 import numpy as np
 
-def leapfrog(I,V,Rl,Rg,Eg,L,C,dz,dt):
+def leapfrog(parameters):
+
+    P = parameters
     """
-    I: current
-    V: voltage
-    Rl: resistance at load
-    Rg: resistance at generator
-    L: p.u.l. inductance
-    C: p.u.l. capacitance
-    dz: space step
-    dt: time step
+    l:  length of the line [m]
+    v:  signal speed [m/s]
+    Zc: characteristic impedance of the line [Ohm]
+    Zg: generator impedance [Ohm]
+    Zl: load impedance [Ohm]
+    Cl: load capacitance [F]
+    A:  amplitude of the voltage source [V]
+    Tb: bit duration [s]
+    tr: bit rise time [s]
+    d:  bit delay [s]
+    N:  number of cells 
+    dt: time step [s]
+    M:  number of time steps
     """
+    l, v, Zc, Zg, Zl, Cl = P[0], P[1], P[2], P[3], P[4], P[5]
+    A, Tb, tr, d = P[6], P[7], P[8], P[9]
+    N, dt, M = int(P[10]), P[11], int(P[12])
+
+    # define adjusted current and voltage
+    V = np.zeros((N+1,M+1))
+    i = np.zeros((N,M)) # remark i = Zc*I
 
     # define constants for leapfrog scheme
 
-    v = 1/(L*C)**(1/2)
+    dz = l/N
     alpha = v*dt/dz
-    Rc = (L/C)**(1/2)
-    rg, rl = Rc/Rg, Rc/Rl# adjusted Rc with respect to Rg and RL
-    C1, C2 = (1-alpha*rg)/(1+alpha*rg), 2*alpha(1+alpha*rg) # constants for BC at z = 0
-    C3, C4 = (1-alpha*rl)/(1+alpha*rl), 2*alpha(1+alpha*rl) # constants for BC at z = N*dz
+    rg, rl = Zc/Zg, Zc/Zl# adjusted Rc with respect to Rg and RL
+    C1, C2 = (1-alpha*rg)/(1+alpha*rg), 2*alpha/(1+alpha*rg) # constants for BC at z = 0
+    C3, C4 = (1-alpha*rl)/(1+alpha*rl), 2*alpha/(1+alpha*rl) # constants for BC at z = N*dz
 
-    N, M = I.shape()
-    i = Rc*I # adjusted I
+    # define mathematical expressions
+
+    H = lambda t: 1*np.greater(t, 0) # heavie side function
+    def B(a,b):
+        # block function: a < b
+        return lambda t: H(t-a) - H(t-b)
+
+    # define source signal
+
+    t1, t2, t3, t4 = d, d+tr, d+tr+Tb, d+2*tr+Tb
+    eg = lambda t: A/tr*(t-t1)*B(t1, t2)(t) + A*B(t2,t4)(t) - A/tr*(t-t3)*B(t3,t4)(t)
 
     for m in range(M-1):
 
-        V[0,m+1] = C1*V[0,m] + C2*(rg*Eg[m] - i[0,m])
+        V[0,m+1] = C1*V[0,m] + C2*(rg*eg((m+0.5)*dt) - i[0,m])
         i[:,m] = i[:,m-1] + alpha*(V[:N,m] - V[1:,m])
         V[1:N,m+1] = V[1:N,m] + alpha*(i[0:N-1,m] - i[1:N,m])
         V[N,m+1] = C3*V[N,m] + C4*i[N-1,m]
 
-    I = 1/Rc*i # readjust i to I
-
-    return I, V
+    I = 1/Zc*i # readjust i to I
+    t = dt*np.arange(M+1) # time for plotting
+    z = dz*np.arange(N+1) # space for plotting
+    
+    return I, V, z, t
---
+++ Electromagnetism_I/Project/python_code/main.py
@@ -0,0 +1,38 @@
+# python modules
+import argparse
+import os
+
+# self made modules
+import leapfrog_scheme as ls
+import io_manager as io
+import plot_manager as pm
+
+# define argument parsers
+parser = argparse.ArgumentParser(description='Input data file to run programm.')
+parser.add_argument('file', type=str, help='Enter the file in which the data is contained to run the leapfrog scheme.', nargs='?', default=None)
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+    file = args.file
+
+    if file is None:
+        print("No file argument was given.")
+        file = input("Please enter a file: ")
+
+    parameters = io.readdata_numpy(file)
+    I, V, z, t = ls.leapfrog(parameters[:-2])
+
+    """
+    Z:  position of the sensor [m]
+    T: snapshot time
+    """
+    Z, T = parameters[-2], parameters[-1]
+    n, m = int(Z/z[1]), int(T)
+    pm.plot_V_at_t(V,z,t[1]*T,m)
+    pm.plot_V_at_z(V,t,Z,n)
+
+
+
+
+
